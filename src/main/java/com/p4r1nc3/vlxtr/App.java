81cc8d9e817156d3f29b5d68e6b1d35470fcaa10
parince <konrad.tupta@gmail.com>
2025-02-10 15:40:55 +0100

    Initial commit

---
+++ src/main/java/com/p4r1nc3/vlxtr/App.java
@@ -0,0 +1,13 @@
+package com.p4r1nc3.vlxtr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
