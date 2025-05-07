facbcecb3b9f897db0907a7a3cf126cb650259a3
P4r1nc3 <konrad.tupta@gmail.com>
2025-05-07 17:56:50 +0200

    Changed the order

--- swagger/swagger-initializer.js
+++ swagger/swagger-initializer.js
@@ -1,12 +1,12 @@
 window.onload = function() {
   window.ui = SwaggerUIBundle({
     urls: [
-      {url: "vlxtr-allegro-api/vlxtr-allegro-api.yaml", name: "VLXTR Allegro API"},
       {url: "vlxtr-api-gateway/vlxtr-api-gateway.yaml", name: "VLXTR API Gateway"},
-      {url: "vlxtr-bambu-api/vlxtr-bambu-api.yaml", name: "VLXTR Bambu API"},
-      {url: "vlxtr-core-api/vlxtr-core-api.yaml", name: "VLXTR Core API"}
+      {url: "vlxtr-allegro-api/vlxtr-allegro-api.yaml", name: "VLXTR Allegro API"},
+      {url: "vlxtr-core-api/vlxtr-core-api.yaml", name: "VLXTR Core API"},
+      {url: "vlxtr-bambu-api/vlxtr-bambu-api.yaml", name: "VLXTR Bambu API"}
     ],
-    "urls.primaryName": "VLXTR Allegro API",
+    "urls.primaryName": "VLXTR API Gateway",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
